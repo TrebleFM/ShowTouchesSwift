b2c8076c70550c533a2bba4eead555e03e9211cb
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2016-11-16 23:31:45 -0500

    Set up bare-bones structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# VisibleTouchViewSwift
+> Show finger touches on the screen using either a UIView or UIWindow. Useful for app preview videos, live product demos, and more.
---
+++ VisibleTouchViewSwift/VisibleTouchWindow.swift
@@ -0,0 +1,23 @@
+//
+//  VisibleTouchView.swift
+//  VisibleTouchViewSwift
+//
+//  Created by Benjamin Chrobot on 11/16/16.
+//  Copyright © 2016 Treble Media, Inc. All rights reserved.
+//
+
+import Foundation
+
+public class VisibleTouchWindow: UIWindow {
+
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+
+        // stub
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
