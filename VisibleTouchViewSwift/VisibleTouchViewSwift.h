718e428e5f629235a3174a42ddd8563649b4233f
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2016-11-16 21:35:01 -0500

    Initial Commit

---
+++ VisibleTouchViewSwift/VisibleTouchViewSwift.h
@@ -0,0 +1,19 @@
+//
+//  VisibleTouchViewSwift.h
+//  VisibleTouchViewSwift
+//
+//  Created by Benjamin Chrobot on 11/16/16.
+//  Copyright © 2016 Treble Media, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VisibleTouchViewSwift.
+FOUNDATION_EXPORT double VisibleTouchViewSwiftVersionNumber;
+
+//! Project version string for VisibleTouchViewSwift.
+FOUNDATION_EXPORT const unsigned char VisibleTouchViewSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisibleTouchViewSwift/PublicHeader.h>
+
+
